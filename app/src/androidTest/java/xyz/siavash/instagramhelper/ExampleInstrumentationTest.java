d8cc4eaa58ceba4b802ded897641a977bab99d58
siavash <siavash.abdoli@gmail.com>
2016-06-29 12:25:43 +0430

    check errorview is visible when error occurred.

--- app/src/androidTest/java/xyz/siavash/instagramhelper/ExampleInstrumentationTest.java
+++ app/src/androidTest/java/xyz/siavash/instagramhelper/ExampleInstrumentationTest.java
@@ -1,16 +1,28 @@
 package xyz.siavash.instagramhelper;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.MediumTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
 import xyz.siavash.instagramhelper.mvp.presenter.RelatedUserPresenter;
+import xyz.siavash.instagramhelper.mvp.ui.TabContainerActivity;
+import xyz.siavash.instagramhelper.util.UserDataPreferences;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -27,10 +39,19 @@ public class ExampleInstrumentationTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
         assertEquals("xyz.siavash.instagramhelper", appContext.getPackageName());
     }
-//    @Test
-//    public void checkRxRequest() throws Exception{
-//        Context appContext = InstrumentationRegistry.getTargetContext();
-//        RelatedUserPresenter relatedUserPresenter=new RelatedUserPresenter();
-//        relatedUserPresenter.loadUsers(false);
-//    }
+    @Rule
+    public ActivityTestRule<TabContainerActivity> activityRule
+            = new ActivityTestRule<>(
+            TabContainerActivity.class,
+            true,     // initialTouchMode
+            true);
+    @Test
+    public void checkErrorVisibilityWithoutToken() throws Exception{
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        UserDataPreferences.setToken("token");
+        onView(withId(R.id.errorView))
+        .check(matches(isDisplayed()));
+    }
+
+
 }
